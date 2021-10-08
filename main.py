8a14d7d448c2f1ba9a05e92d54dc7b5781b0bff9
Dustin Heckmann <kontakt@dustin-heckmann.de>
2021-10-08 20:27:02 +0200

    Refactor calc_check_digit

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-def calc_check_digit(number: int):
-
-    digits = [i for i in str(number)]
-    print(digits)
-    odds = digits[-1::-2]
-    evens = digits[-2::-2]
-    odds_sum = sum([int(i)*3 for i in odds])
-    evens_sum = sum([int(i) for i in evens])
-
-    return (10-((odds_sum+evens_sum)%10))%10
+def calc_check_digit(digits: str) -> int:
+    odds_sum = sum_digits(digits[-1::-2])
+    evens_sum = sum_digits(digits[-2::-2])
+    return (10 - ((odds_sum * 3 + evens_sum) % 10)) % 10
 
 
-if __name__ == '__main__':
-    calc_check_digit(1234567)
+def sum_digits(digits: str) -> int:
+    return sum(map(int, digits))
