bed55330b50ee110055b7c447477704ce00f584a
Eleftherios Matios <e.matios@gmail.com>
2021-10-08 21:02:52 +0200

    Add functions for encoding

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ def calc_check_digit(digits: str) -> int:
 
 def sum_digits(digits: str) -> int:
     return sum(map(int, digits))
+
+
+def l_code_digit(digit: str) -> str:
+    l_codes = {"0": "0001101"}
+    return l_codes[digit]
+
+
+def r_code_digit(digit: str) -> str:
+    l_code = l_code_digit(digit)
+    return "".join(["0" if bit == "1" else "1" for bit in l_code])
+
+
+def g_code_digit(digit: str) -> str:
+    r_code = r_code_digit(digit)
+    return r_code[::-1]
