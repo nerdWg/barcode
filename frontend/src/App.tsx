793fdb81dff1636c1ca4d29670a92e6b68656b1d
Dustin Heckmann <kontakt@dustin-heckmann.de>
2021-10-30 09:27:54 +0200

    Add EAN13 code to web app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import './App.css';
 import { Barcode } from './Barcode';
 
 function App() {
   return (
     <div className="App">
       <h1>Barcode</h1>
-      <Barcode type="ean8" code="00000000"/>
-    
-    
+      <Barcode type="ean8" code="1311411"/>
+      <Barcode type="ean13" code="000000000000"/>
     </div>
   );
 }
--- frontend/src/Barcode.tsx
+++ frontend/src/Barcode.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-
+import { useCallback, useEffect, useState } from "react";
+type BarcodeType = "ean8" | "ean13";
 interface Props {
-  type: string;
+  type: BarcodeType;
   code: string;
 }
 
@@ -9,47 +9,53 @@ export const Barcode = ({ type, code }: Props) => {
   const [binary, setBinary] = useState<string>();
   const [barcode, setBarcode] = useState<string>();
 
-  const update = async () => {
-    const bin = await fetchBinaryCode(code);
+  const update = useCallback(async () => {
+    const bin = await fetchBinaryCode(code, type);
     setBinary(bin);
 
-    const barcode = await fetchBarCode(code);
+    const barcode = await fetchBarCode(code, type);
     setBarcode(barcode);
-  };
+  }, [type, code]);
 
   useEffect(() => {
     update();
-  });
+  }, [type, code, update]);
 
   return (
     <table>
-      <tr>
-        <th>type</th> <td>{type}</td>
-      </tr>
-      <tr>
-        <th>code</th> <td>{code}</td>
-      </tr>
-
-      <tr>
-        <th>binary</th> <td>{binary}</td>
-      </tr>
-      <tr>
-        <th>barcode</th>
-        <td
-          className="content"
-          dangerouslySetInnerHTML={{ __html: barcode ?? "" }}
-        ></td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>type</th>
+          <td>{type}</td>
+        </tr>
+        <tr>
+          <th>code</th>
+          <td>{code}</td>
+        </tr>
+        <tr>
+          <th>binary</th>
+          <td>{binary}</td>
+        </tr>
+        <tr>
+          <th>barcode</th>
+          <td
+            className="content"
+            dangerouslySetInnerHTML={{ __html: barcode ?? "" }}
+          ></td>
+        </tr>
+      </tbody>
     </table>
   );
 };
 
-const fetchBinaryCode = async (code: string) => {
-  const response = await fetch(`/barcode8/${code}`);
+const fetchBinaryCode = async (code: string, type: BarcodeType) => {
+  const url = type === "ean8" ? "barcode8" : "barcode13";
+  const response = await fetch(`/${url}/${code}`);
   return await response.text();
 };
 
-const fetchBarCode = async (code: string) => {
-  const response = await fetch(`/bc8_svg/${code}`);
+const fetchBarCode = async (code: string, type: BarcodeType) => {
+  const url = type === "ean8" ? "bc8_svg" : "bc13_svg";
+  const response = await fetch(`/${url}/${code}`);
   return await response.text();
 };
