85f03563e8f0bdb6d4c2721d23580ad7fd16a7e6
Dustin Heckmann <kontakt@dustin-heckmann.de>
2021-10-30 18:08:55 +0200

    Rename backend.py to app.py to avoid having to set FLASK_APP variable

--- backend/run_contract_tests.sh
+++ backend/run_contract_tests.sh
@@ -3,7 +3,7 @@
 PACT_LOCATION="../frontend/pact/pacts/frontend-backend.json"
 FLASK_PORT=8989
 
-FLASK_APP=backend poetry run flask run --port $FLASK_PORT > /dev/null 2> /dev/null &
+poetry run flask run --port $FLASK_PORT > /dev/null 2> /dev/null &
 FLASK_PID=$!
 
 poetry run pact-verifier --provider-base-url=http://localhost:$FLASK_PORT --pact-url=$PACT_LOCATION
