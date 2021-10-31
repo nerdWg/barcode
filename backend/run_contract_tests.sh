86a60ff72c556ce8c8cd31baa02a9a6835f7994e
Dustin Heckmann <kontakt@dustin-heckmann.de>
2021-10-31 10:54:40 +0100

    Set FLASK_ENV to development in contract tests

--- backend/run_contract_tests.sh
+++ backend/run_contract_tests.sh
@@ -3,7 +3,8 @@
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 
 PACT_LOCATION="../frontend/pact/pacts/frontend-backend.json"
-FLASK_PORT=8989
+FLASK_PORT=41809
+export FLASK_ENV=development
 
 poetry run flask run --port $FLASK_PORT > /dev/null 2> /dev/null &
 FLASK_PID=$!
