3f847a06c57f94a57333cb9afe466a15269d6fa3
Eleftherios Matios <e.matios@gmail.com>
2021-10-31 07:40:10 +0100

    Fix kill command, add better signal handling

--- backend/run_contract_tests.sh
+++ backend/run_contract_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+
 PACT_LOCATION="../frontend/pact/pacts/frontend-backend.json"
 FLASK_PORT=8989
 
@@ -7,4 +9,4 @@ poetry run flask run --port $FLASK_PORT > /dev/null 2> /dev/null &
 FLASK_PID=$!
 
 poetry run pact-verifier --provider-base-url=http://localhost:$FLASK_PORT --pact-url=$PACT_LOCATION
-KILL $FLASK_PID
+kill $FLASK_PID
