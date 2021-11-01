9b407fee04829bc1a3ba69834cb94110747c2429
Eleftherios Matios <e.matios@gmail.com>
2021-11-01 16:01:53 +0100

    Fix startup script

--- start-app-tmux.sh
+++ start-app-tmux.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-session="barcode"
+SESSION="barcode"
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
-if [ "$SESSIONEXISTS" = "" ]
-	tmux new-session -d -s $session
-	tmux send-keys 'cd backend' C-m 'poetry run flask run' C-m
-	tmux split-window -v
-	tmux send-keys 'cd frontend' C-m 'yarn start' C-m
+if [ "$SESSIONEXISTS" = "" ]; then
+  tmux new-session -d -s $SESSION
+  tmux send-keys 'cd backend' C-m 'FLASK_ENV=development poetry run flask run' C-m
+  tmux split-window -v
+  tmux send-keys 'cd frontend' C-m 'yarn start' C-m
 fi
 tmux attach-session -t $SESSION:0
